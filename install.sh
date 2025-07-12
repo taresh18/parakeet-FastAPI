465987262b1866c65fc81029cb1a9b17689b2cb3
Taresh Rajput <58368119+taresh18@users.noreply.github.com>
2025-07-12 15:17:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ set -e
 # use runpod/pytorch:2.1.0-py3.10-cuda11.8.0-devel-ubuntu22.04 docker images in runpod
 
 # apt packages
-apt-get update && apt-get -y install libopenmpi-dev python3-venv nano htop
+apt-get update && apt-get -y install libopenmpi-dev python3-venv nano htop ffmpeg
 cd /workspace
 git clone https://github.com/taresh18/parakeet-FastAPI.git && cd parakeet-FastAPI
-python3 -m venv parakeet && source parakeet/bin/activate 
+python3 -m venv parakeet && source parakeet/bin/activate
+pip install numpy typing_extensions # required for sox build
 pip install -r requirements.txt
 chmod +x start.sh && bash -x start.sh
